de9e452ba062462ae1b7a3bdf558f4ccb6ed3bbb
MBATheGamer <mbathegamer@gmail.com>
2026-05-18 11:20:00 +0100

    Initialize backend

---
+++ backend/src/test/java/com/mbathegamer/budgetto/BudgettoApplicationTests.java
@@ -0,0 +1,10 @@
+package com.mbathegamer.budgetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgettoApplicationTests {
+	@Test
+	void contextLoads() {}
+}
