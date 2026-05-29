b20bb5e115e38a4e15a943af8844ec7d04094b8b
MBATheGamer <mbathegamer@gmail.com>
2026-05-29 22:50:12 +0100

    Add token validation login to `JwtService`

--- backend/src/main/java/com/mbathegamer/budgetto/services/JwtService.java
+++ backend/src/main/java/com/mbathegamer/budgetto/services/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -23,5 +24,19 @@ public class JwtService {
         .expiration(new Date(System.currentTimeMillis() + 1_000 * tokenExpiration))
         .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
         .compact();
+  }
+
+  public boolean validateToken(String token) {
+    try {
+      var claims = Jwts.parser()
+          .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+          .build()
+          .parseSignedClaims(token)
+          .getPayload();
+
+      return claims.getExpiration().after(new Date());
+    } catch (JwtException exception) {
+      return false;
+    }
   }
 }
