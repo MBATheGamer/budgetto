ca67ce37eaf9ab515290b5159ab5c7facee8b784
MBATheGamer <mbathegamer@gmail.com>
2026-05-29 23:14:54 +0100

    Add get an email from token logic inside `JwtService`

--- backend/src/main/java/com/mbathegamer/budgetto/services/JwtService.java
+++ backend/src/main/java/com/mbathegamer/budgetto/services/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -28,15 +29,21 @@ public class JwtService {
 
   public boolean validateToken(String token) {
     try {
-      var claims = Jwts.parser()
-          .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
-          .build()
-          .parseSignedClaims(token)
-          .getPayload();
-
-      return claims.getExpiration().after(new Date());
+      return getClaims(token).getExpiration().after(new Date());
     } catch (JwtException exception) {
       return false;
     }
+  }
+
+  private Claims getClaims(String token) {
+    return Jwts.parser()
+        .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  public String getEmailFromToken(String token) {
+    return getClaims(token).getSubject();
   }
 }
