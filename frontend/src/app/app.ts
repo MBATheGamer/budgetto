327cb033bdff1ebad762b682e2e833c66dd90d3c
TejBenamor <tejeddin2002@gmail.com>
2026-06-01 13:50:01 +0100

    Add `health` check API integration

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,23 @@
-import { Component, signal } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
-  imports: [],
-  template: "<h1>Hello, World!</h1>",
+  imports: [RouterOutlet],
+  template: "<router-outlet />",
 })
 export class App {
   protected readonly title = signal("budgetto");
+  public response = signal("");
+  private http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get<{status: string}>("http://localhost:8080/api/v1/health")
+    .subscribe({
+      next: (data) => {
+        this.response.set(data.status);
+      }
+    })
+  }
 }
