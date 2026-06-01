cf806b1a1a3af10e77b7116d2551c605ae5343bc
TejBenamor <tejeddin2002@gmail.com>
2026-06-01 13:54:28 +0100

    Add routing for sign-up components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from "@angular/router";
+import { SignUp } from "./sign-up";
+import { SignIn } from "./sign-in";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "sign-up",
+    component: SignUp,
+  },
+];
