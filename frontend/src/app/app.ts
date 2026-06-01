243188e525bdceb17a996d127477131f85b3578c
TejBenamor <tejeddin2002@gmail.com>
2026-06-01 21:31:08 +0100

    remove fetching from /health API, it's for test purpose only

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, inject, signal } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ToastComponent } from "./toast.component";
 
@@ -10,14 +9,4 @@ import { ToastComponent } from "./toast.component";
 })
 export class App {
   protected readonly title = signal("budgetto");
-  public response = signal("");
-  private http = inject(HttpClient);
-
-  ngOnInit() {
-    this.http.get<{ status: string }>("http://localhost:8080/api/v1/health").subscribe({
-      next: (data) => {
-        this.response.set(data.status);
-      },
-    });
-  }
 }
