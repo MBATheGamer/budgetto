a3f3c355943e4f61e44f7afa2f7273724d10062a
MBATheGamer <mbathegamer@gmail.com>
2026-06-02 11:44:02 +0100

    Add `GuestAuth` guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { Dashboard } from "./dashboard";
 import { AuthGuard } from "./guards/auth.guard";
+import { GuestGuard } from "./guards/guest.guard";
 import { SignIn } from "./sign-in";
 import { SignUp } from "./sign-up";
 
@@ -8,10 +9,12 @@ export const routes: Routes = [
   {
     path: "sign-up",
     component: SignUp,
+    canActivate: [GuestGuard],
   },
   {
     path: "sign-in",
     component: SignIn,
+    canActivate: [GuestGuard],
   },
   {
     path: "dashboard",
