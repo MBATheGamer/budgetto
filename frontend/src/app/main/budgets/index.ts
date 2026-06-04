674dfa29dfcd948faa1d95c96668502dab943373
TejBenamor <tejeddin@ankaboot.io>
2026-06-04 12:27:04 +0100

    Add `budget` component

---
+++ frontend/src/app/main/budgets/index.ts
@@ -0,0 +1,58 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, inject, OnInit, signal } from "@angular/core";
+import { BudgetModal } from "../../components/budget-modal";
+import { DeleteModal } from "../../components/delete-modal";
+import { BudgetService } from "../../services/budget.service";
+import { ToastService } from "../../services/toast.service";
+import { Budget, BudgetRequest } from "../../types";
+
+@Component({
+  selector: "app-budgets",
+  imports: [BudgetModal, DeleteModal],
+  templateUrl: "./index.html",
+})
+export class Budgets implements OnInit {
+  private readonly budgetService = inject(BudgetService);
+  private readonly toastService = inject(ToastService);
+
+  public budgets = signal<Budget[]>([]);
+  public title = signal("Delete a budget");
+  public content = signal("");
+
+  budgetId: number | null = null;
+
+  ngOnInit() {
+    this.budgetService.getAll().subscribe({
+      next: (res) => this.budgets.set(res),
+      error: (error: HttpErrorResponse) => this.toastService.show(error.error, "error"),
+    });
+  }
+
+  onSaved(budget: Budget) {
+    const index = this.budgets().findIndex((b) => b.id === budget.id);
+    if (index !== -1) {
+      this.budgets.update((list) => list.map((b) => (b.id === budget.id ? budget : b)));
+    } else {
+      this.budgets.update((list) => [...list, budget]);
+    }
+  }
+
+  confirmDelete() {
+    if (this.budgetId) {
+      this.budgetService.delete(this.budgetId).subscribe({
+        error: (error: HttpErrorResponse) => this.toastService.show(error.error, "error"),
+      });
+      this.budgets.set(this.budgets().filter((b) => b.id !== this.budgetId));
+    }
+  }
+
+  updateRequest(budget: Budget | null, modal: BudgetModal) {
+    modal.open(budget);
+  }
+
+  deleteRequest(budget: Budget, modal: DeleteModal) {
+    this.budgetId = budget.id;
+    this.content.set(`Are you sure you want to delete this budget?`);
+    modal.open();
+  }
+}
